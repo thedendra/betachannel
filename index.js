5e026854ff5d957c841482c512e2a226ef41234c
Δένδρα - Dendra <96614062+thedendra@users.noreply.github.com>
2021-12-29 23:18:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -78,15 +78,90 @@ function clrmbmenu()
     setTimeout(() => { document.getElementById("mobshwn").style.display = "none"; }, 400);
 }
 
-function mobmenutxtpad(Lnum, Fsize)
+
+
+
+function mobmenutxtpad(Fsize)
 {
     var rot = document.querySelector(':root');
-    
+    var Lnum = document.getElementsByClassName("txtt").length;
 
     var res = (95 - (Lnum * Fsize)) / (Lnum + 1);
     var pad = res + 'vmax';
 
     rot.style.setProperty('--mob-menu-txt-pad', pad);
-
+    
     return pad;
 }
+
+function cenpar()
+{
+    var toCenr = document.querySelectorAll(".mottor");
+    var toCenl = document.querySelectorAll(".mottol");
+    var noAnim = document.querySelectorAll(".mottopar");
+    
+    for(var i = 0; i < toCenr.length; i++)
+    {
+        toCenr[i].classList.add("mottocen");
+        toCenr[i].classList.remove("mottor");
+    }
+
+    for(var i = 0; i < toCenl.length; i++)
+    {
+        toCenl[i].classList.add("mottocen");
+        toCenl[i].classList.remove("mottol");
+    }
+
+    for(var i = 0; i < toCenl.length; i++)
+    {
+        noAnim[i].classList.add("provv");
+        noAnim[i].classList.remove("mottopar");
+    }
+    
+    return toCenl.length;
+}
+
+function setpar()
+{
+    var toSet = document.querySelectorAll(".mottocen");
+    var toAnim = document.querySelectorAll(".provv");
+
+    for(let i = 0; i < toSet.length; i++)
+    {
+        if(i % 2 == 0)
+        {
+            toSet[i].classList.add("mottol");
+            toSet[i].classList.remove("mottocen");
+        }
+        else
+        {
+            toSet[i].classList.add("mottor");
+            toSet[i].classList.remove("mottocen");
+        }
+    }
+
+    for(var i = 0; i < toAnim.length; i++)
+    {
+        toAnim[i].classList.add("mottopar");
+        toAnim[i].classList.remove("provv");
+    }
+    
+}
+
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+function sizeList(e)
+{
+    if(e.matches)
+    {
+        cenpar();
+    }
+    else
+    {
+        setpar();
+    }
+}
+
+mediaQuery.addListener(sizeList);
+
+sizeList(mediaQuery);
